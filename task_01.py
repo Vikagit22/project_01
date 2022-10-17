9ddb85a580d77d13c263bc4f212402de100a706e
Vikagit22 <vikarast@mail.ru>
2022-10-17 08:38:11 +0300

    добавлен еще один комментарий

--- task_01.py
+++ task_01.py
@@ -7,3 +7,5 @@ my_favorite_songs = 'Waste a Moment, Staying\' Alive, A Sorta Fairytale, Start M
 
 # ответ на задание
 print(my_favorite_songs[0:14],my_favorite_songs[64:77],my_favorite_songs[16:30],my_favorite_songs[51:62])
+# верен ли ответ?
+
