61fef333cd878ab681156c2b360c38b552e0c09f
Vikagit22 <vikarast@mail.ru>
2022-10-23 16:11:47 +0300

    изменено решение задачи №7

--- probe.py
+++ probe.py
@@ -1,2 +1,59 @@
 print('thank you')
 print('кажется получается')
+my_art1=titles[my_keys[i]]
+sales[my_art1]
+quantity = 0
+for dict in sales[my_art1]:
+    quantity += dict['quantity'] 
+    price = 0
+for dict in sales[my_art1]:
+    price += dict['price']
+print(f{my_keys[i]} - {quantity} шт, стоимость {price} 'руб')
+i=i+1
+print("получилось")
+
+my_art1=titles['Мячик тип 2 - Adidas']
+sales[my_art1]
+quantity = 0
+for dict in sales[my_art1]:
+    quantity += dict['quantity'] 
+    price = 0
+for dict in sales[my_art1]:
+    price += dict['price']
+print(f'Мячик тип 2 - Adidas - {quantity} шт, стоимость {price} руб')
+my_art1=titles['Кепка тип 1 - Adidas']
+sales[my_art1]
+quantity = 0
+for dict in sales[my_art1]:
+    quantity += dict['quantity'] 
+    price = 0
+for dict in sales[my_art1]:
+    price += dict['price']
+print(f'Кепка тип 1 - Adidas - {quantity} шт, стоимость {price} руб')
+my_art1=titles['Ремень тип 2 - Nike']
+sales[my_art1]
+quantity = 0
+for dict in sales[my_art1]:
+    quantity += dict['quantity'] 
+    price = 0
+for dict in sales[my_art1]:
+    price += dict['price']
+print(f'Ремень тип 2 - Nike - {quantity} шт, стоимость {price} руб')
+my_art1=titles['Футболка тип 1 - Adidas']
+sales[my_art1]
+quantity = 0
+for dict in sales[my_art1]:
+    quantity += dict['quantity'] 
+    price = 0
+for dict in sales[my_art1]:
+    price += dict['price']
+print(f'Футболка тип 1 - Adidas - {quantity} шт, стоимость {price} руб')
+my_art1=titles['Шапка тип 5 - Puma']
+sales[my_art1]
+quantity = 0
+for dict in sales[my_art1]:
+    quantity += dict['quantity'] 
+    price = 0
+for dict in sales[my_art1]:
+    price += dict['price']
+print(f'Шапка тип 5 - Puma - {quantity} шт, стоимость {price} руб')
--- task_07.py
+++ task_07.py
@@ -38,57 +38,17 @@ sales = {
 
 # Есть словарь списков количества товаров на складе.
 
-my_art1=titles['Кроссовки тип 3 - Adidas']
-sales[my_art1]
-quantity = 0
-for dict in sales[my_art1]:
-    quantity += dict['quantity'] 
+my_keys =list(titles)
+i=0
+while i<=len(my_keys)-1:
+    i2= my_keys[i]
+    my_art1=titles[i2]
+    sales[my_art1]
+    quantity = 0
+    for dict in sales[my_art1]:
+        quantity += dict['quantity'] 
     price = 0
-for dict in sales[my_art1]:
-    price += dict['price']
-print(f'Кроссовки тип 3 - Adidas - {quantity} шт, стоимость {price} руб')
-my_art1=titles['Мячик тип 2 - Adidas']
-sales[my_art1]
-quantity = 0
-for dict in sales[my_art1]:
-    quantity += dict['quantity'] 
-    price = 0
-for dict in sales[my_art1]:
-    price += dict['price']
-print(f'Мячик тип 2 - Adidas - {quantity} шт, стоимость {price} руб')
-my_art1=titles['Кепка тип 1 - Adidas']
-sales[my_art1]
-quantity = 0
-for dict in sales[my_art1]:
-    quantity += dict['quantity'] 
-    price = 0
-for dict in sales[my_art1]:
-    price += dict['price']
-print(f'Кепка тип 1 - Adidas - {quantity} шт, стоимость {price} руб')
-my_art1=titles['Ремень тип 2 - Nike']
-sales[my_art1]
-quantity = 0
-for dict in sales[my_art1]:
-    quantity += dict['quantity'] 
-    price = 0
-for dict in sales[my_art1]:
-    price += dict['price']
-print(f'Ремень тип 2 - Nike - {quantity} шт, стоимость {price} руб')
-my_art1=titles['Футболка тип 1 - Adidas']
-sales[my_art1]
-quantity = 0
-for dict in sales[my_art1]:
-    quantity += dict['quantity'] 
-    price = 0
-for dict in sales[my_art1]:
-    price += dict['price']
-print(f'Футболка тип 1 - Adidas - {quantity} шт, стоимость {price} руб')
-my_art1=titles['Шапка тип 5 - Puma']
-sales[my_art1]
-quantity = 0
-for dict in sales[my_art1]:
-    quantity += dict['quantity'] 
-    price = 0
-for dict in sales[my_art1]:
-    price += dict['price']
-print(f'Шапка тип 5 - Puma - {quantity} шт, стоимость {price} руб')
+    for dict in sales[my_art1]:
+        price += dict['price']
+    print(f'{my_keys[i]}, {quantity} шт, стоимость {price} руб')
+    i=i+1
