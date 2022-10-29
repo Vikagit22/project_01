700574464b15bccc2b25bed057739aadf2068dca
Nikita Pyshkov <85805562+pyshkovni@users.noreply.github.com>
2022-10-29 22:47:46 +0300

    Добавил решение

--- task_03.py
+++ task_03.py
@@ -13,3 +13,25 @@ if month <= 12:
     print('в месяце',my_year[month-1][1],'дн')
 else:
     print('Вы ввели некорректный номер месяца')
+
+# Довольно интересный список!
+# Также можно сделать следующим образом
+# Решение 1
+if month in [1, 3, 5, 7, 8, 10, 12]:
+    print(31)
+elif month in [4, 6, 9, 11]:
+    print(30)
+elif month == 2:
+    print(28)
+else:
+    print('Введен неправильный номер месяца!')
+
+# Решение 2
+import calendar as cl  # используем модуль для получения функции
+
+year_input = input("Введите год: ")
+month_input = input("Введите номер месяца: ")
+
+year = int(year_input)
+month_ = int(month_input)
+print(cl.monthrange(year, month_))
